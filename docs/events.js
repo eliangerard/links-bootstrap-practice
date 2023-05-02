79770ce1f0bb46c3c2978a3b2edd0544a064f18c
Elian <eliangerardiso@gmail.com>
2023-05-01 21:40:12 -0600

    Ignorar, es para poder ver la página en https://eliangerard.me/links-bootstrap-practice/

---
+++ docs/events.js
@@ -0,0 +1,8 @@
+const changeBackground = () => {
+    const btnBackground = document.getElementById("cardBackground");
+    btnBackground.classList.add("new-card-background");
+}
+const changeBorde = () => {
+    const btnBorde = document.getElementById("card");
+    btnBorde.classList.add("new-border");
+}
